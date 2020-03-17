824a211822c5a79f8a6cc12214a03e3cf98b0474
amparosap <a.sapena.2019@alumnos.urjc.es>
2020-03-17 12:41:21 +0100

    ex1: Modify the __ini__ methond of the Seq class so that it detects that the given string only have these four valid bases: 'A', 'C', 'G' and 'T'. If a different character is found, the sequence should be initilalized with the "ERROR" string, and the message "INCORRECT Sequence detected" printed on the console

--- Session-06/Ex1.py
+++ Session-06/Ex1.py
@@ -1,18 +1,27 @@
 class Seq:
     """A class for representing sequence objects"""
     def __init__(self, strbases):
-        self.strbases =strbases
+        self.strbases = strbases
+        for el in strbases:
+            if el not in ["A", "C", "T", "G"]:
+                print ("ERROR: Invalid seq detected")
+                self.strbases= "ERROR"
+                return
+
         print("New sequence created ")
+
 
     def __str__(self):
         return self.strbases
 
+    def len(self):
+        return len(self.strbases)
+    pass
 
-
+class Gene(Seq):
+    pass
 s1 = Seq("ACCTGC")
 s2 = Seq("Hello? Am I a valid sequence?")
-
 #mainmenu
-
 print(f"Sequence 1: {s1}")
 print(f"Sequence 2: {s2}")
