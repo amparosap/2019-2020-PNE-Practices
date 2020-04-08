1100c9ae4f079eb527b0b1424755012a1997d717
amparosap <a.sapena.2019@alumnos.urjc.es>
2020-04-08 09:57:52 +0200

    server session8

--- P2/Client0.py
+++ P2/Client0.py
@@ -11,3 +11,16 @@ class Client:
 
     def __str__(self):
         return (f"Connection to SERVER at {self.ip}, PORT: {self.port}")
+
+    def talk(self, msg):
+        response = ''
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.ip, self.port))
+        # Send data.
+        s.send(str.encode(msg))
+        # Receive data
+        response += s.recv(2048).decode("utf-8")
+        s.close()
+        return (response)
---
+++ P2/Ex3.py
@@ -0,0 +1,15 @@
+from Client0 import  Client
+
+PRACTICE = 2
+EXERCISE = 3
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "192.168.1.45"
+PORT = 8080
+
+c = Client(IP, PORT)
+print("Sending a message to the server...")
+response = c.talk("Testing !!!")
+print(f"Response: {response}")
