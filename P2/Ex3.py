d198bebacd103492aae436f28ed65de162920896
amparosap <a.sapena.2019@alumnos.urjc.es>
2020-04-27 11:03:45 +0200

    UPDATE

--- P2/Ex3.py
+++ P2/Ex3.py
@@ -1,4 +1,4 @@
-from Client0 import  Client
+from Client0 import Client
 
 PRACTICE = 2
 EXERCISE = 3
@@ -6,10 +6,13 @@ EXERCISE = 3
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
-IP = "192.168.1.45"
+IP = "127.0.0.1"
 PORT = 8080
 
 c = Client(IP, PORT)
+print(c)
+
+# -- Send a message to the server
 print("Sending a message to the server...")
-response = c.talk("Testing !!!")
+response = c.talk("Testing!!!")
 print(f"Response: {response}")
