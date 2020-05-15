a8f7c4f3f12a52fe681166a221e4ee8760883504
amparosap <a.sapena.2019@alumnos.urjc.es>
2020-05-15 15:42:47 +0200

    Updated!

--- P0/Ex8.py
+++ P0/Ex8.py
@@ -4,6 +4,14 @@ print("------Exercise 8--------")
 folder ="../session-04/.."
 bases=["A","C","T","G"]
 files_list=["U5","ADA","FRAT1","FXN","RNU6_269P"]
+txt = ".txt"
 
 for line in files_list:
-    c
+    val = 0
+    base = ''
+    for i, t in seq_count(seq_read_fasta(FOLDER + e + txt)).items():
+        while t > val:
+            val = t
+            base = i
+    print("Gene ", e, " : Most frequent base: ", base)
+
